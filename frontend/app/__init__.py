570fee518b11f70d9e7a365b1589dd211df2e28c
Sreenivas Gundu <srinivasgundu70@yahoo.com>
2022-10-09 13:27:16 +0530

    Sign in web page and routes for email login, google login, forgot_password, register, logout along with login_required wrapper

--- frontend/app/__init__.py
+++ frontend/app/__init__.py
@@ -1,6 +1,24 @@
 from flask import Flask
 from flask import send_from_directory
+from authlib.integrations.flask_client import OAuth
+
 app = Flask(__name__,instance_relative_config=True,static_url_path='')
+
+
+app.config['SECRET_KEY'] = 'secret_key'
+'''OAuth Configuration'''
+oauth = OAuth()
+oauth.init_app(app)
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth.register(
+            name='google',
+            client_id= "43033882869-6u8msl41v0oi764cajeer3e9bf60cj2j.apps.googleusercontent.com",
+            client_secret = "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",
+            server_metadata_url=CONF_URL,
+            client_kwargs={
+                'scope': 'openid email profile'
+            }
+        )
 
 
 @app.route('/site.webmanifest')
