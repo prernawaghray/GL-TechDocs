2d94096b9d0170b8cd9158df6b1a47b4e4ae2ebc
Sarith-Madhu <108563460+Sarith-Madhu@users.noreply.github.com>
2022-10-20 18:58:02 +0530

    Update prodhelpers.js

--- frontend/app/static/js/prodhelpers.js
+++ frontend/app/static/js/prodhelpers.js
@@ -1,4 +1,4 @@
-var rootApiUrl = "http://localhost:56733/api/";
+var rootApiUrl = "http://localhost:6622/api/";
 var rootFrontEndUrl = "http://localhost:56733/";
 function getApiUrl(api)
 {
@@ -36,5 +36,12 @@ function parseJwt (token) {
     return JSON.parse(jsonPayload);
 };
 
+function getResponseMessage(data)
+{
+    if("responseJSON" in data && "message" in data.responseJSON)
+        return data.responseJSON.message;
+    else
+        return data.statusText;
+}
 
 
