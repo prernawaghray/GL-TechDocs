bf327fa948205ed0d008e037313f49c9e7aeef74
Mugunthan045 <mugunthan1711045@gmail.com>
2022-10-28 10:18:55 +0530

    Updated test scripts

--- backend/TestScripts/loginTest.py
+++ backend/TestScripts/loginTest.py
@@ -2,7 +2,17 @@ import requests
 import sys
 
 
-req = requests.post('http://127.0.0.1:5000/api/signin', json={'loginType':'email','username':'test3@test.com','password':'1234567', 'rememberMe':1})
-
+req = requests.post('http://127.0.0.1:5000/api/signin', json={'loginType':'email','email':'test@test.com','password':'1234567', 'rememberMe':1})
 print(req.json())
-#eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpc0FkbWluIjpmYWxzZX0.7d4LVnr43Od8tK7tcCdMMRoieXYD8xycLJBxxQxSDOU
+
+##Below response - "User not Registered"
+req = requests.post("http://127.0.0.1:5000/api/signin", json={'loginType':'gmail', 'email':'test@test.com', 'password':'1234567', 'rememberMe':1})
+print(req.json())
+
+##Below response - "Email field cannot be empty"
+req = requests.post("http://127.0.0.1:5000/api/signin", json={'loginType':'email', 'email':'', 'password':'1234567', 'rememberMe':1})
+print(req.json())
+
+##Below repsonse - "Password field cannot be empty"
+req = requests.post("http://127.0.0.1:5000/api/signin", json={'loginType':'email', 'email':'test@test.com', 'password':'', 'rememberMe':1})
+print(req.json())
--- backend/TestScripts/userProfileTesting.py
+++ backend/TestScripts/userProfileTesting.py
@@ -16,27 +16,29 @@
 
 ##For updateProfile
 import requests
-json={ 
+import json
+json_obj={ 
         'userData':{
             'firstName':'new',
-            'lastName':'new2'
-        },
-        'address':{
-            'streetAddress':'newStreeatt',
-            'state': 'newSte',
-            'country': 'newCountry',
-        },
-        'occupation':'newoccupation',
-        'purposeOfUse':'NewUsage'
+            'lastName':'new89789789',
+                    
+            'address':{
+                'streetAddress':'newStreeatt',
+                'state': 'newSte',
+                'country': 'newCountry',
+                    },
+            'occupation':'newoccupation',
+            'purposeOfUse':'NewUsage'
     
+        }
     }
 headers = {"authToken":'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpc0FkbWluIjpmYWxzZX0.7d4LVnr43Od8tK7tcCdMMRoieXYD8xycLJBxxQxSDOU'}
-req = requests.post('http://127.0.0.1:5000/api/updateProfile', json=json, headers=headers)
+req = requests.post('http://127.0.0.1:5000/api/updateProfile', json=json_obj, headers=headers)
 print(req.json())
 
 ##For GetProfile
 req = requests.post('http://127.0.0.1:5000/api/getProfile', headers=headers)
-print(req.json())
+print(json.dumps(req.json(), indent=3))
 
 #####METHOD NOT ALLOWED#######
 req =requests.get("http://127.0.0.1:5000/api/getProfile", headers=headers)
