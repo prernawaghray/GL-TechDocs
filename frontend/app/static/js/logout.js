5779bbb1191e89eeaa506d0d34f614db5b83f0ce
Srinivas Gundu <srinivasgundu70@yahoo.com>
2022-11-05 11:32:47 +0530

    added clearSession() function in logout.js to clear frontend session on logout

--- frontend/app/static/js/logout.js
+++ frontend/app/static/js/logout.js
@@ -1,14 +1,35 @@
-/* this logoutButtonClicked function checks for the userToken in local Storage
+/* This logoutButtonClicked function checks for the userToken in local Storage
+   and user in Session Storage. 
+   if user in Session Storage it clears Session and 
    if JWT is i local storage it removes it sends JWT to backend for validation
    if it is valid redirects to home page after logout
    if it is invalid or not in local storage, it issues a "bad request" alert and 
    redirects to home page*/
 
-   
+
+async function clearSession() {
+
+    await fetch(getFrontEndUrl('clearSession'), {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => {console.log(data)})
+    .then(error => {console.log(error)})
+        
+    return
+}
+
+
+
 async function logoutButtonClicked()
 {
     
     userToken = localStorage.getItem('userToken');
+    sessionUser = sessionStorage.getItem('user');
+    
+    if (sessionUser !== null){
+        clearSession()
+    }
     
     if (userToken !== null){
         localStorage.removeItem('userToken');
