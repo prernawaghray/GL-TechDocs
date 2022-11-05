f0cd87a70da2b80622244e52c72962197c60ca19
Shreya Sood <shreyasood297@gmail.com>
2022-11-06 00:41:02 +0530

    updated as per review comment

--- backend/services/ConvertLatexToPdx/ConvertLatexToPdf.py
+++ backend/services/ConvertLatexToPdx/ConvertLatexToPdf.py
@@ -1,11 +1,13 @@
 # Store this code in 'app.py' file
+import os
 import pathlib
 import stat
-import warnings
-
-from flask import Flask, request, Blueprint
-import os, subprocess, platform
+import subprocess
 import sys
+import warnings
+from os.path import exists
+
+from flask import request, Blueprint, make_response, jsonify, current_app
 
 # Suppress warnings
 warnings.filterwarnings("ignore")
@@ -19,34 +21,32 @@ convertLatexToPdfBlueprint = Blueprint('convertLatexToPdfBlueprint', __name__)
 def convertToPdf():
     # TeX source filename
     tex_filename = request.args.get('filepath')
-    filename, ext = os.path.splitext(tex_filename)
-    # the corresponding PDF filename
-    pdf_filename = filename + '.pdf'
-    log_filename = filename + '.log'
-    aux_filename = filename + '.aux'
-    path = pathlib.Path(tex_filename).parent
-    os.chmod(path, stat.S_IRWXO)
-    print(pdf_filename)
+    file_exists = exists(tex_filename)
+    if file_exists:
+        filename, ext = os.path.splitext(tex_filename)
+        # the corresponding PDF filename
+        pdf_filename = filename + '.pdf'
+        log_filename = filename + '.log'
+        aux_filename = filename + '.aux'
+        path = pathlib.Path(tex_filename).parent
+        os.chmod(path, stat.S_IRWXO)
+        print(pdf_filename)
 
-    # compile TeX file
-    subprocess.run('pdflatex -interaction=nonstopmode -no-shell-escape ' + tex_filename, shell=True, check=True)
+        # compile TeX file
+        subprocess.run('pdflatex -interaction=nonstopmode -no-shell-escape ' + tex_filename, shell=True, check=True)
 
-    # check if PDF is successfully generated
-    if not os.path.exists(pdf_filename):
-        raise RuntimeError('PDF output not found')
-    if os.path.exists(log_filename):
-        os.remove(log_filename)
-    if os.path.exists(aux_filename):
-        os.remove(aux_filename)
+        if os.path.exists(log_filename):
+            os.remove(log_filename)
+        if os.path.exists(aux_filename):
+            os.remove(aux_filename)
 
-    # open PDF with platform-specific command
-    if platform.system().lower() == 'darwin':
-        subprocess.run(['open', pdf_filename])
-    elif platform.system().lower() == 'windows':
-        os.startfile(pdf_filename)
-    elif platform.system().lower() == 'linux':
-        subprocess.run(['xdg-open', pdf_filename])
+        # check if PDF is successfully generated
+        if not os.path.exists(pdf_filename):
+            current_app.logger.info('Converted PDF file could not be found. Latex to PDF conversion failed for the '
+                                    'file: ' + tex_filename)
+            return make_response(jsonify('Latex to PDF conversion failed for the file: ' + tex_filename), 500)
+        current_app.logger.info('Latex to PDF conversion successful for the file: ' + tex_filename)
+        return make_response(jsonify(pdf_filename), 202)
     else:
-        raise RuntimeError('Unknown operating system "{}"'.format(platform.system()))
-
-    return pdf_filename
+        current_app.logger.info('Provided Latex filepath ' + tex_filename + ' not found.')
+        return make_response(jsonify(tex_filename), 404)
