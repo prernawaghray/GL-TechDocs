abd83cbc5a07b9a918d3b04a500d9300ffc3f9ae
rajakannak5 <rajakanna.k5@gmail.com>
2022-11-07 19:43:20 +0530

    echo details

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 app="techdocs-frontend"
+port=80
 docker build -t ${app} .
-docker run -d -p 56733:80 \
+docker run -d -p ${port}:80 \
   --name=${app} \
   -v $PWD:/app ${app}
+ 
+echo "Serving on :${port}"
